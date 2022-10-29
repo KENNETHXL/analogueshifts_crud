defce88b23617cf1b2551edf618c4c11bdeff4b6
usman <usmaarn@gmail.com>
2022-10-29 03:24:26 +0100

    added vite manifest

---
+++ public/build/assets/View.d82f9305.js
@@ -0,0 +1,1 @@
+import{j as r,a as e,H as o}from"./app.4a29d4e0.js";import{A as s}from"./AllLayout.89d74823.js";import"./Navbar.4bc59927.js";import"./iconBase.9998dff7.js";import"./index.esm.68e4cabd.js";function n(i){return r(s,{children:[e(o,{title:"Welcome"}),e("div",{className:"home min-h-screen"})]})}export{n as default};
